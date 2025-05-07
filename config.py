8b7444137b216239d5659e43043e37dd6c9799b4
gr3go001 <vjptmbfarioli@gmail.com>
2025-05-07 16:24:00 -0300

    adiciona variaveis de ambiente render

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://aterra_user:c3zgMh4oL9Wn4Hn4RQe0ThierNPEGyEH@dpg-d0dna515pdvs739cjjqg-a/aterradb"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'uma-string-supersecreta-padrao')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'qZ9f&b2$Vm@94h!Xu8#sL0eP')

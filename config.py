4e48aa4641d6d09de609629c9d4a11da4055d58d
gr3go001 <vjptmbfarioli@gmail.com>
2025-05-07 12:04:35 -0300

    config db

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://joao:impossivel999@localhost/agenda'
+    SQLALCHEMY_DATABASE_URI = "postgresql://aterra_user:c3zgMh4oL9Wn4Hn4RQe0ThierNPEGyEH@dpg-d0dna515pdvs739cjjqg-a/aterradb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'uma-string-supersecreta-padrao')
